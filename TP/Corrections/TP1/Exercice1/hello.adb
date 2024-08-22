4963d5d6503ae8a827fc4d4d1dcf6cd4aa86c0e4
Franck Silvestre <franck.silvestre@gmail.com>
2024-08-22 14:15:35 +0200

    fix 1.1.11 hello.adb OK

---
+++ TP/Corrections/TP1/Exercice1/hello.adb
@@ -0,0 +1,14 @@
+-- import the IO library tha will be use
+with ada.text_io; use ada.text_io;
+
+-- declare the procedure hello : the main program
+procedure hello is
+
+-- start the main program
+begin
+
+  -- print the string "Hello" on the console
+  put("Hello");
+
+-- end of the main program
+end hello;
